94c6790c0b75c31b8867c828e2769ea75cad1cf8
Alex Modugno <AlexModugno2024@u.northwestern.edu>
2023-09-25 19:07:25 -0500

    Add a Firebase database

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Banner from "./components/Banner";
-import CourseList from "./components/CourseList";
-import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.css";
 import TermPage from "./components/TermPage";
 import Form from "./components/Form";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useJsonQuery } from "./utilities/fetch";
-
-const queryClient = new QueryClient();
+import { useDbData } from "./utilities/firebase";
 
 import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 
 const Main = () => {
-  const [data, isLoading, error] = useJsonQuery(
-    "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php"
-  );
+  const [data, error] = useDbData("/");
   const [selectedClasses, setSelectedClasses] = useState([]);
 
   if (error) return <h1>Error loading user data: {`${error}`}</h1>;
-  if (isLoading) return <h1>Loading user data...</h1>;
   if (!data) return <h1>No user data found</h1>;
 
   return (
@@ -59,11 +50,9 @@ const Main = () => {
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <Main />
-      </div>
-    </QueryClientProvider>
+    <div className="App">
+      <Main />
+    </div>
   );
 };
 
